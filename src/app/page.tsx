1e862e79556db98730707df60b32e46b4a8c7a7a
Briankimp <bkimathi973@gmail.com>
2024-09-13 00:11:40 +0300

    Made a few changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Pagination from "../components/Pagination";
-// Sample data - replace this with your actual data source
+
 const sampleData = Array.from({ length: 50 }, (_, i) => ({
   id: i + 1,
   name: `Item ${i + 1}`,
@@ -10,19 +10,19 @@ const sampleData = Array.from({ length: 50 }, (_, i) => ({
 const ITEMS_PER_PAGE = 10; // Define the number of items per page
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(1); // State for the current page
+  const [currentPage, setCurrentPage] = useState(1); 
 
-  // Calculate total number of pages
+
   const totalPages = Math.ceil(sampleData.length / ITEMS_PER_PAGE);
 
-  // Calculate the data to be displayed based on the current page  
+
   const currentData = sampleData.slice(
     (currentPage - 1) * ITEMS_PER_PAGE,
     currentPage * ITEMS_PER_PAGE
   );
   return (
     <div className="container  mx-auto p-4  ">
-      {/* Render current data */}
+      <h1 className="text-2xl font-bold text-center">Pagination Example</h1>
       <div className="grid gap-4  ">
         {currentData.map((item) => (
           <div key={item.id} className="p-4 bg-white text-black  shadow rounded">
